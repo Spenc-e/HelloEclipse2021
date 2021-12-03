1105718ad25eaaf77400e97540497faab7f630a9
Spencer Walton <swalton1@unb.ca>
2021-12-03 14:57:04 -0400

    Project Commit V2

--- HelloProject/src/HelloApp.java
+++ HelloProject/src/HelloApp.java
@@ -4,6 +4,6 @@ public class HelloApp {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.print("Hello Mr.Walton! I was wondering if you could...");
-		//Second
+		//For some reason this isnt working
 	}
 }
